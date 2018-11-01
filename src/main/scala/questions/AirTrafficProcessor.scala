4c40f92fdb629c6db0d9d5f0bf1167cc00e8603a
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-11-01 23:44:13 +0200

    implement loaddataandregister

--- src/main/scala/questions/AirTrafficProcessor.scala
+++ src/main/scala/questions/AirTrafficProcessor.scala
@@ -1,59 +1,56 @@
 package questions
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 /** AirTrafficProcessor provides functionalites to
-* process air traffic data
-* Spark SQL tables available from start:
-*   - 'carriers' airliner information
-*   - 'airports' airport information
-*
-* After implementing the first method 'loadData'
-* table 'airtraffic',which provides flight information,
-* is also available. You can find the raw data from /resources folder.
-* Useful links:
-* http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.sql.Dataset
-* http://spark.apache.org/docs/latest/sql-programming-guide.html
-* https://github.com/databricks/spark-csv
-*
-* We are using data from http://stat-computing.org/dataexpo/2009/the-data.html
-*
-* @param spark reference to SparkSession
-* @param filePath path to the air traffic data
-* @param airportsPath path to the airport data
-* @param carriersPath path to the carrier data
-*/
+  * process air traffic data
+  * Spark SQL tables available from start:
+  *   - 'carriers' airliner information
+  *   - 'airports' airport information
+  *
+  * After implementing the first method 'loadData'
+  * table 'airtraffic',which provides flight information,
+  * is also available. You can find the raw data from /resources folder.
+  * Useful links:
+  * http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.sql.Dataset
+  * http://spark.apache.org/docs/latest/sql-programming-guide.html
+  * https://github.com/databricks/spark-csv
+  *
+  * We are using data from http://stat-computing.org/dataexpo/2009/the-data.html
+  *
+  * @param spark        reference to SparkSession
+  * @param filePath     path to the air traffic data
+  * @param airportsPath path to the airport data
+  * @param carriersPath path to the carrier data
+  */
 class AirTrafficProcessor(spark: SparkSession,
-    filePath:String, airportsPath: String, carriersPath: String) {
+                          filePath: String, airportsPath: String, carriersPath: String) {
 
-    import spark.implicits._
+  /*
+  * Table names for SQL
+  * use airTraffic for table name in loadData
+  * method.
+  */
+  val airTraffic = "airtraffic"
+  val carriers = "carriers"
+  val airports = "airports"
 
-    /*
-    * Table names for SQL
-    * use airTraffic for table name in loadData
-    * method.
-    */
-    val airTraffic = "airtraffic"
-    val carriers = "carriers"
-    val airports = "airports"
+  // load the files and create tables
+  // for spark sql
+  //DO NOT EDIT
+  val carriersTable: DataFrame = spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv(carriersPath)
+  carriersTable.createOrReplaceTempView(carriers)
+  //DO NOT EDIT
+  val airportsTable: DataFrame = spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv(airportsPath)
+  airportsTable.createOrReplaceTempView(airports)
 
-    // load the files and create tables
-    // for spark sql
-    //DO NOT EDIT
-    val carriersTable = spark.read
-        .option("header","true")
-        .option("inferSchema", "true")
-        .csv(carriersPath)
-    carriersTable.createOrReplaceTempView(carriers)
-    //DO NOT EDIT
-    val airportsTable = spark.read
-        .option("header","true")
-        .option("inferSchema", "true")
-        .csv(airportsPath)
-    airportsTable.createOrReplaceTempView(airports)
-
-    /** load the data and register it as a table
+  /** load the data and register it as a table
     * so that we can use it later for spark SQL.
     * File is in csv format, so we are using
     * Spark csv library.
@@ -64,86 +61,91 @@ class AirTrafficProcessor(spark: SparkSession,
     * https://github.com/databricks/spark-csv
     *
     * Note:
-    *   If you just load the data using 'inferSchema' -> 'true'
-    *   some of the fields which should be Integers are casted to
+    * If you just load the data using 'inferSchema' -> 'true'
+    * some of the fields which should be Integers are casted to
     *   Strings. That happens, because NULL values are represented
-    *   as 'NA' strings in this data.
+    * as 'NA' strings in this data.
     *
     *   E.g:
-    *   2008,7,2,3,733,735,858,852,DL,1551,N957DL,85,77,42,6,-2,CAE,
-    *   ATL,191,15,28,0,,0,NA,NA,NA,NA,NA
+    * 2008,7,2,3,733,735,858,852,DL,1551,N957DL,85,77,42,6,-2,CAE,
+    * ATL,191,15,28,0,,0,NA,NA,NA,NA,NA
     *
-    *   Therefore you should remove 'NA' strings and replace them with
+    * Therefore you should remove 'NA' strings and replace them with
     *   NULL. Option 'nullValue' in csv library is useful. However you
-    *   don't need to take empty Strings into account.
-    *   For instance TailNum field contains empty strings.
+    * don't need to take empty Strings into account.
+    * For instance TailNum field contains empty strings.
     *
     * Correct Schema:
-    *   |-- Year: integer (nullable = true)
-    *   |-- Month: integer (nullable = true)
-    *   |-- DayofMonth: integer (nullable = true)
-    *   |-- DayOfWeek: integer (nullable = true)
-    *   |-- DepTime: integer (nullable = true)
-    *   |-- CRSDepTime: integer (nullable = true)
-    *   |-- ArrTime: integer (nullable = true)
-    *   |-- CRSArrTime: integer (nullable = true)
-    *   |-- UniqueCarrier: string (nullable = true)
-    *   |-- FlightNum: integer (nullable = true)
-    *   |-- TailNum: string (nullable = true)
-    *   |-- ActualElapsedTime: integer (nullable = true)
-    *   |-- CRSElapsedTime: integer (nullable = true)
-    *   |-- AirTime: integer (nullable = true)
-    *   |-- ArrDelay: integer (nullable = true)
-    *   |-- DepDelay: integer (nullable = true)
-    *   |-- Origin: string (nullable = true)
-    *   |-- Dest: string (nullable = true)
-    *   |-- Distance: integer (nullable = true)
-    *   |-- TaxiIn: integer (nullable = true)
-    *   |-- TaxiOut: integer (nullable = true)
-    *   |-- Cancelled: integer (nullable = true)
-    *   |-- CancellationCode: string (nullable = true)
-    *   |-- Diverted: integer (nullable = true)
-    *   |-- CarrierDelay: integer (nullable = true)
-    *   |-- WeatherDelay: integer (nullable = true)
-    *   |-- NASDelay: integer (nullable = true)
-    *   |-- SecurityDelay: integer (nullable = true)
-    *   |-- LateAircraftDelay: integer (nullable = true)
+    * |-- Year: integer (nullable = true)
+    * |-- Month: integer (nullable = true)
+    * |-- DayofMonth: integer (nullable = true)
+    * |-- DayOfWeek: integer (nullable = true)
+    * |-- DepTime: integer (nullable = true)
+    * |-- CRSDepTime: integer (nullable = true)
+    * |-- ArrTime: integer (nullable = true)
+    * |-- CRSArrTime: integer (nullable = true)
+    * |-- UniqueCarrier: string (nullable = true)
+    * |-- FlightNum: integer (nullable = true)
+    * |-- TailNum: string (nullable = true)
+    * |-- ActualElapsedTime: integer (nullable = true)
+    * |-- CRSElapsedTime: integer (nullable = true)
+    * |-- AirTime: integer (nullable = true)
+    * |-- ArrDelay: integer (nullable = true)
+    * |-- DepDelay: integer (nullable = true)
+    * |-- Origin: string (nullable = true)
+    * |-- Dest: string (nullable = true)
+    * |-- Distance: integer (nullable = true)
+    * |-- TaxiIn: integer (nullable = true)
+    * |-- TaxiOut: integer (nullable = true)
+    * |-- Cancelled: integer (nullable = true)
+    * |-- CancellationCode: string (nullable = true)
+    * |-- Diverted: integer (nullable = true)
+    * |-- CarrierDelay: integer (nullable = true)
+    * |-- WeatherDelay: integer (nullable = true)
+    * |-- NASDelay: integer (nullable = true)
+    * |-- SecurityDelay: integer (nullable = true)
+    * |-- LateAircraftDelay: integer (nullable = true)
     *
     * @param path absolute path to the csv file.
     * @return created DataFrame with correct column types.
     */
-    def loadDataAndRegister(path: String): DataFrame = {
-        ???
-    }
+  def loadDataAndRegister(path: String): DataFrame = {
+    spark.read.format("csv")
+      .option("nullValue", "NA")
+      .option("treatEmptyValuesAsNulls", value = false)
+      .option("inferSchema", value = true)
+      .option("header", value = true)
+      .load(path)
+  }
 
-    //USE can use SPARK SQL or DataFrame transformations
+  //USE can use SPARK SQL or DataFrame transformations
 
-    /** Gets the number of flights for each
+  /** Gets the number of flights for each
     * airplane. 'TailNum' column is unique for each
     * airplane so it should be used. DataFrame should
     * also be sorted by count in descending order.
     *
     * Result looks like:
-    *   +-------+-----+
-    *   |TailNum|count|
-    *   +-------+-----+
-    *   | N635SW| 2305|
-    *   | N11150| 1342|
-    *   | N572UA| 1176|
-    *   | N121UA|    8|
-    *   +-------+-----+
+    * +-------+-----+
+    * |TailNum|count|
+    * +-------+-----+
+    * | N635SW| 2305|
+    * | N11150| 1342|
+    * | N572UA| 1176|
+    * | N121UA|    8|
+    * +-------+-----+
     *
     * @param df Air traffic data
     * @return DataFrame containing number of flights per
     * TailNum. DataFrame is sorted by count. Column names
-    * are TailNum and count
+    *         are TailNum and count
     */
-    def flightCount(df: DataFrame): DataFrame = {
-        ???
-    }
+  def flightCount(df: DataFrame): DataFrame = {
+    ???
+  }
 
 
-    /** Which flights were cancelled due to
+  /** Which flights were cancelled due to
     * security reasons the most?
     *
     * Example output:
@@ -157,14 +159,14 @@ class AirTrafficProcessor(spark: SparkSession,
     * +---------+----+
     *
     * @return Returns a DataFrame containing flights which were
-    * cancelled due to security reasons (CancellationCode = 'D').
-    * Columns FlightNum and Dest are included.
+    *         cancelled due to security reasons (CancellationCode = 'D').
+    *         Columns FlightNum and Dest are included.
     */
-    def cancelledDueToSecurity(df: DataFrame): DataFrame = {
-        ???
-    }
+  def cancelledDueToSecurity(df: DataFrame): DataFrame = {
+    ???
+  }
 
-    /** What was the longest weather delay between January
+  /** What was the longest weather delay between January
     * and march (1.1-31.3)?
     *
     * Example output:
@@ -175,13 +177,13 @@ class AirTrafficProcessor(spark: SparkSession,
     * +----+
     *
     * @return DataFrame containing the highest delay which
-    * was due to weather.
+    *         was due to weather.
     */
-    def longestWeatherDelay(df: DataFrame): DataFrame = {
-        ???
-    }
+  def longestWeatherDelay(df: DataFrame): DataFrame = {
+    ???
+  }
 
-    /** Which airliners didn't fly.
+  /** Which airliners didn't fly.
     * Table 'carriers' has this information.
     *
     * Example output:
@@ -197,11 +199,11 @@ class AirTrafficProcessor(spark: SparkSession,
     *
     * @return airliner descriptions
     */
-    def didNotFly(df: DataFrame): DataFrame = {
-        ???
-    }
+  def didNotFly(df: DataFrame): DataFrame = {
+    ???
+  }
 
-    /** Find the airliners which travel
+  /** Find the airliners which travel
     * from Vegas to JFK. Sort them in descending
     * order by number of flights and append the
     * DataFrame with the data from carriers.csv.
@@ -209,26 +211,26 @@ class AirTrafficProcessor(spark: SparkSession,
     * Vegas iasa code: LAS
     * JFK iasa code: JFK
     *
-    *   Output should look like:
+    * Output should look like:
     *
-    *   +--------------------+----+
-    *   |         Description| Num|
-    *   +--------------------+----+
-    *   |     JetBlue Airways|1824|
-    *   |Delta Air Lines Inc.|1343|
-    *   |US Airways Inc. (...| 948|
-    *   |American Airlines...| 366|
-    *   +--------------------+----+
+    * +--------------------+----+
+    * |         Description| Num|
+    * +--------------------+----+
+    * |     JetBlue Airways|1824|
+    * |Delta Air Lines Inc.|1343|
+    * |US Airways Inc. (...| 948|
+    * |American Airlines...| 366|
+    * +--------------------+----+
     *
     * @return DataFrame containing Columns Description
-    * (airliner name) and Num(number of flights) sorted
-    * in descending order.
+    *         (airliner name) and Num(number of flights) sorted
+    *         in descending order.
     */
-    def flightsFromVegasToJFK(df: DataFrame): DataFrame = {
-        ???
-    }
+  def flightsFromVegasToJFK(df: DataFrame): DataFrame = {
+    ???
+  }
 
-    /** How much time airplanes spend on moving from
+  /** How much time airplanes spend on moving from
     * gate to the runway and vise versa at the airport on average.
     * This method should return a DataFrame containing this
     * information per airport.
@@ -241,25 +243,25 @@ class AirTrafficProcessor(spark: SparkSession,
     *
     *
     * DataFrame should look like:
-    *   +-------+-----------------+
-    *   |airport|             taxi|
-    *   +-------+-----------------+
-    *   |    BRW|5.084736087380412|
-    *   |    OME|5.961294471783976|
-    *   |    OTZ|6.866595496262391|
-    *   |    DAL|6.983973195822733|
-    *   |    HRL|7.019248180512919|
-    *   |    SCC|7.054629009320311|
-    *   +-------+-----------------+
+    * +-------+-----------------+
+    * |airport|             taxi|
+    * +-------+-----------------+
+    * |    BRW|5.084736087380412|
+    * |    OME|5.961294471783976|
+    * |    OTZ|6.866595496262391|
+    * |    DAL|6.983973195822733|
+    * |    HRL|7.019248180512919|
+    * |    SCC|7.054629009320311|
+    * +-------+-----------------+
     *
     * @return DataFrame containing time spend on taxiing per
-    * airport ordered in ascending order.
+    *         airport ordered in ascending order.
     */
-    def timeSpentTaxiing(df: DataFrame): DataFrame = {
-        ???
-    }
+  def timeSpentTaxiing(df: DataFrame): DataFrame = {
+    ???
+  }
 
-    /** What is the median travel distance?
+  /** What is the median travel distance?
     * Field Distance contains this information.
     *
     * Example output:
@@ -271,11 +273,11 @@ class AirTrafficProcessor(spark: SparkSession,
     *
     * @return DataFrame containing the median value
     */
-    def distanceMedian(df: DataFrame): DataFrame = {
-        ???
-    }
+  def distanceMedian(df: DataFrame): DataFrame = {
+    ???
+  }
 
-    /** What is the carrier delay, below which 95%
+  /** What is the carrier delay, below which 95%
     * of the observations may be found?
     *
     * Example output:
@@ -287,12 +289,12 @@ class AirTrafficProcessor(spark: SparkSession,
     *
     * @return DataFrame containing the carrier delay
     */
-    def score95(df: DataFrame): DataFrame = {
-        ???
-    }
+  def score95(df: DataFrame): DataFrame = {
+    ???
+  }
 
 
-    /** From which airport are flights cancelled the most?
+  /** From which airport are flights cancelled the most?
     * What percentage of flights are cancelled from a specific
     * airport?
     * cancelledFlights combines flight data with
@@ -307,43 +309,42 @@ class AirTrafficProcessor(spark: SparkSession,
     *
     *
     *
-    *   Output should look something like this:
-    *   +--------------------+--------------------+--------------------+
-    *   |             airport|                city|          percentage|
-    *   +--------------------+--------------------+--------------------+
-    *   |  Telluride Regional|           Telluride|   0.211340206185567|
-    *   |  Waterloo Municipal|            Waterloo| 0.17027863777089783|
-    *   |Houghton County M...|             Hancock| 0.12264150943396226|
-    *   |                Adak|                Adak| 0.09803921568627451|
-    *   |Aspen-Pitkin Co/S...|               Aspen| 0.09157716223855286|
-    *   |      Sioux Gateway |          Sioux City| 0.09016393442622951|
-    *   +--------------------+--------------------+--------------------+
+    * Output should look something like this:
+    * +--------------------+--------------------+--------------------+
+    * |             airport|                city|          percentage|
+    * +--------------------+--------------------+--------------------+
+    * |  Telluride Regional|           Telluride|   0.211340206185567|
+    * |  Waterloo Municipal|            Waterloo| 0.17027863777089783|
+    * |Houghton County M...|             Hancock| 0.12264150943396226|
+    * |                Adak|                Adak| 0.09803921568627451|
+    * |Aspen-Pitkin Co/S...|               Aspen| 0.09157716223855286|
+    * |      Sioux Gateway |          Sioux City| 0.09016393442622951|
+    * +--------------------+--------------------+--------------------+
     *
     * @return DataFrame containing columns airport, city and percentage
     */
-    def cancelledFlights(df: DataFrame): DataFrame = {
-        ???
-    }
+  def cancelledFlights(df: DataFrame): DataFrame = {
+    ???
+  }
 
-    /**
+  /**
     * Calculates the linear least squares approximation for relationship
     * between DepDelay and WeatherDelay.
     *  - First filter out entries where DepDelay < 0
     *  - there are definitely multiple data points for a single
-    *    DepDelay value so calculate the average WeatherDelay per
-    *    DepDelay
+    * DepDelay value so calculate the average WeatherDelay per
+    * DepDelay
     *  - Calculate the linear least squares (c+bx=y) where
-    *    x equals DepDelay and y WeatherDelay. c is the constant term
-    *    and b is the slope.
+    * x equals DepDelay and y WeatherDelay. c is the constant term
+    * and b is the slope.
     *
-    *  
     * @return tuple, which has the constant term first and the slope second
     */
-    def leastSquares(df: DataFrame):(Double, Double) = {
-        ???
-    }
+  def leastSquares(df: DataFrame): (Double, Double) = {
+    ???
+  }
 
-    /**
+  /**
     * Calculates the running average for DepDelay per day.
     * Average should be taken from both sides of the day we
     * are calculating the running average. You should take
@@ -399,16 +400,17 @@ class AirTrafficProcessor(spark: SparkSession,
     *
     *
     * @return DataFrame with schema 'date' (YYYY-MM-DD) (string type) and 'moving_average' (double type)
-    * ordered by 'date'
+    *         ordered by 'date'
     */
-    def runningAverage(df: DataFrame): DataFrame = {
-        ???
-    }
+  def runningAverage(df: DataFrame): DataFrame = {
+    ???
+  }
 }
+
 /**
-*
-*  Change the student id
-*/
+  *
+  * Change the student id
+  */
 object AirTrafficProcessor {
-    val studentId = "728272"
+  val studentId = "728272"
 }
--- src/main/scala/questions/Main.scala
+++ src/main/scala/questions/Main.scala
@@ -1,28 +1,26 @@
 package questions
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
 
 object Main {
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
 
     Logger.getLogger("org").setLevel(Level.OFF)
-      Logger.getLogger("akka").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
 
     val spark = SparkSession.builder
       .master("local[*]")
       .appName("main")
       .config("spark.driver.memory", "5g")
-      .config("spark.dynamicAllocation.enabled","true")
-      .config("spark.shuffle.service.enabled","true")
+      .config("spark.dynamicAllocation.enabled", "true")
+      .config("spark.shuffle.service.enabled", "true")
       .getOrCreate()
 
-      val filePath = getClass().getResource("/2008.csv").toString
-      val airportsPath = getClass().getResource("/airports.csv").toString
-      val carriersPath = getClass().getResource("/carriers.csv").toString
+    val filePath = getClass.getResource("/2008.csv").toString
+    val airportsPath = getClass.getResource("/airports.csv").toString
+    val carriersPath = getClass.getResource("/carriers.csv").toString
 
     val processor = new AirTrafficProcessor(spark, filePath, airportsPath, carriersPath)
     val data = processor.loadDataAndRegister(filePath)
@@ -46,7 +44,7 @@ object Main {
     // println("<<<cancelled flights>>>")
     // processor.cancelledFlights().show()
     //println("least squares: " + processor.leastSquares())
-    
+
 
     spark.stop()
   }
